c755ed9307d0d02b2a6d6ff7eef2b549e73b65fc
nerd100 <den.meyer1@web.de>
2016-06-26 00:02:15 +0200

    finished

--- src/Ryu.java
+++ src/Ryu.java
@@ -1,10 +1,6 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.*;
 import java.util.Random;
 import java.util.*;
 
@@ -15,18 +11,33 @@ import gameInterface.AIInterface;
 import commandcenter.CommandCenter;
 
 public class Ryu implements AIInterface {
-
+	BufferedWriter bw = null;
+	Writer writer = null;
+	File file;
+	FileWriter fw;
+	
+	String tempskill;
+	
 	int time = 0;
 	int enemyHealth;
+	int absDamage = 0;
+	int currentDmg = 0;
+	int lastDmg = 0;
+	int fightLine = -1;
+	int line = 0;
+	
 	boolean nextSkill;
 	boolean exist = false;
-	File file;
-	Key inputKey;
-	String tempskill;
 	boolean playerNumber;
+	
+	Key inputKey;
 	FrameData  frameData;
 	CommandCenter cc;
 	
+	List <String> succList = new ArrayList<String>();
+	List <String> fightList = new ArrayList<String>();
+	
+	String[] fightArray;
 	String[] skills = 
 		{"AIR","AIR_A","AIR_B","AIR_D_DB_BA","AIR_D_DB_BB","AIR_D_DF_FA","AIR_D_DF_FB"
 			+ "AIR_DA","AIR_DB","AIR_F_D_DFA","AIR_F_D_DFB","AIR_FA","AIR_FB","AIR_GUARD","AIR_GUARD_RECOV",
@@ -37,27 +48,34 @@ public class Ryu implements AIInterface {
 			"STAND_D_DF_FC","STAND_F_D_DFA","STAND_F_D_DFB","STAND_FA","STAND_FB","STAND_GUARD",  
 			"STAND_GUARD_RECOV","STAND_RECOV","THROW_A","THROW_B","THROW_HIT","THROW_SUFFER"};
 	
-	List <String> succList = new ArrayList<String>();
-	
 	@Override
 	public int initialize(GameData gameData, boolean playerNumber) {
+		this.playerNumber = playerNumber;
+		this.inputKey = new Key();
+		cc = new CommandCenter();
+		frameData = new FrameData();
+		
 		enemyHealth = 0;
 		nextSkill = true;
 		//create file
 		file = new File("data/aiData/SkillTable/skill_table.txt");
 		file.getParentFile().mkdirs();
 		try {
+			if(!file.exists())
 			file.createNewFile();
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		//create file
-		this.playerNumber = playerNumber;
-		this.inputKey = new Key();
-		cc = new CommandCenter();
-		frameData = new FrameData();
-		return 0; 
+		try {
+			fw = new FileWriter(file.getAbsoluteFile(),true);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		bw = new BufferedWriter(fw);
+
+	    return 0; 
 	}
 
 
@@ -70,6 +88,8 @@ public class Ryu implements AIInterface {
 	
 	@Override
 	public void processing(){ 
+		if(frameData.getRound() <= 3){
+			
 		if (time > 100){
 			tempskill = rndcommand(skills);
 			System.out.println(tempskill);
@@ -78,12 +98,92 @@ public class Ryu implements AIInterface {
 
 		fight(tempskill);
 		fillList();
+		
 		if(succList.size() == 5)
 		{
+			succList.add(Integer.toString(absDamage));
+			try {
+				bw.append(succList.toString());
+				bw.newLine();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			System.out.println("write in file");
+			succList.removeAll(succList);
+			absDamage = 0;
 		}
 	time++;
+		}else{
+			BufferedReader reader = null;
+			try {
+				reader = new BufferedReader(new FileReader(file));
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		      List<String> results = new ArrayList<String>();
+		      String line = null;
+			try {
+				line = reader.readLine();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		      while (line != null) {
+		          results.add(line);
+		          try {
+					line = reader.readLine();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		      }
+		      //System.out.println(results);
+		      if(fightLine == -1){
+		      fightLine = whichLine(results);
+		      }
+		      //System.out.println(results.get(fightLine));
+		      if(fightList.isEmpty()){
+		      String temp = results.get(fightLine);
+		      String[] temp1 = temp.split(",");
+		      for (int j = 0; j < temp1.length-1;j++)
+		      	{
+		    
+		    	  temp1[j] = temp1[j].replaceAll("\\s|\\[|\\]", "");
+	    		  fightList.add(temp1[j]);
+	    	  	}
+		      fightArray = fightList.toArray(new String[fightList.size()]);
+		      }
+
+		      tempskill = rndcommand(fightArray);
+		      fight(tempskill);
+		}
 	}
+	
+	public int whichLine(List<String> results)
+	{
+		
+		for (int i = 0 ; i < results.size(); i++)
+	      {
+	    	  //System.out.println(results.get(i));
+	    	  String temp = results.get(i);
+	    	  String[] temp1 = temp.split(",");
+	    	  List <String> temp2 = new ArrayList<String>();
+	    	  for (int j = 0; j < temp1.length;j++)
+	    	  {
+	    		  temp2.add(temp1[j]);
+	    	  }
+	    	  String dmg = temp2.get(2).replaceAll("[\\s\\]]", "");
+	    	  currentDmg = Integer.parseInt(dmg);
+	    	  if(lastDmg < currentDmg){
+	    	  lastDmg = currentDmg;
+	    	  line = i;
+	    	  }
+	      }   	
+		return line;
+	}
+	
 	public void fillList()
 	{
 		if(!frameData.getEmptyFlag()){
@@ -99,6 +199,7 @@ public class Ryu implements AIInterface {
 				}
 				if(exist == false){
 				succList.add(tempskill);
+				absDamage += Math.abs(enemyHealth - cc.getEnemyHP());
 				}
 				exist = false;
 				enemyHealth = cc.getEnemyHP();
@@ -131,6 +232,12 @@ public class Ryu implements AIInterface {
 
 	public void close(){
 		System.out.println(succList);
+		try {
+			bw.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 
